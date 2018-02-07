43df6c6417b138e90c2c0f7144ffa82c3f64d725
Stefan Kruger <stefan.kruger@uk.ibm.com>
2018-02-07 15:54:57 +0000

    Dead code removed in client

--- adaptor/cloudant/client.go
+++ adaptor/cloudant/client.go
@@ -129,11 +129,6 @@ func WithSeqInterval(seqInterval int) ClientOptionFunc {
 
 // Connect wraps the underlying session to the Cloudant database
 func (c *Client) Connect() (client.Session, error) {
-	if c.database == nil {
-		if err := c.initConnection(); err != nil {
-			return nil, err
-		}
-	}
 	return &Session{
 		client:   c.client,
 		dbName:   c.dbName,
